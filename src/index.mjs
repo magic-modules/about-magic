d2ea7a7030e97b8609c0f08d9df7e21435551871
jascha <jascha@jaeh.at>
2020-03-06 23:41:40 +0100

    page/index: update content, add style for headers, format

--- src/index.mjs
+++ src/index.mjs
@@ -2,19 +2,8 @@ export const View = () =>
   div({ class: 'AboutMagic' }, [
     h2([Link({ text: '@magic', to: 'https://magic.github.io' }), ' is']),
 
-    h3([
-      'a ',
-      Link({ text: 'jamstack', to: 'https://jamstack.org/' }),
-      ':',
-    ]),
-    p([
-      b('j'),
-      'avascript, ',
-      b('a'),
-      'pis and (static) ',
-      b('m'),
-      'arkup.',
-    ]),
+    h3(['a ', Link({ text: 'jamstack', to: 'https://jamstack.org/' }), ':']),
+    p([b('j'), 'avascript, ', b('a'), 'pis and (static) ', b('m'), 'arkup.']),
 
     h3('minimal:'),
     p('less than 15kb of html + css + javascript boilerplate.'),
@@ -26,13 +15,17 @@ export const View = () =>
     p('html loads first and provides basic functionality, javascript enhances where needed.'),
 
     h3('full stack:'),
-    p('if needed, @magic generates server side apis.'),
-
-    h3('hosting:'),
     p([
-      Link({ to: 'https://grundstein.it', text: '@grundstein'}),
-      ' (will) make region and provider agnostic hosting of @magic stacks easier.',
+      'if needed, @magic generates server side apis, and ',
+      Link({ text: '@grundstein', to: 'https://grundstein.it' }),
+      ' will soon take care of hosting said app bundles',
     ]),
 
     GitBadges('magic/core'),
   ])
+
+export const style = {
+  h3: {
+    padding: '0.5em 0 0',
+  },
+}
